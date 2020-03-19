a53f502c713a453655e743fc8a702d7120153f92
Cameron <cameronsteele@steelecameron.com>
2020-03-19 16:09:52 -0700

    trying

--- components/App.jsx
+++ components/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from './VisibleCard.jsx';
-import HistoryTime from './history-time';
+import HistoryTime from '../history-time';
 // import Card from './Card.jsx';
 // import '../primary.scss';
 
@@ -16,39 +16,49 @@ import {
 
 // require('../primary.css');
 
-const App = () => (
-	<div>
-		<Card nature="text">
-			<h1>Cameron Steele</h1>
-			<h2>developer</h2>
-			<h3>designer</h3>
-			<a className="email" href="mailto:cameronsteele@steelecameron.com">
-				cameronsteele@steelecameron.com
-			</a><br />
-			{/*<!-- <a className="phone" href="tel:+13479797266">347-979-7266</a> -->*/}
-			{/*<!-- <a href="https://github.com/cameronsteele"><i className="fab fa-github"></i></a> -->*/}
-		</Card>
+// HistoryTime.bindPathToCallback('/', function;
+// HistoryTime
 
-		<KINDSnacks />
-		<CloudCred />
-		<Telephenesis />
-		<FoundSoundNation />
-		<EchoSign />
-		<ColonieNYC />
-		{/*<BismuthBear />*/}
-		{/*<GoldenCeph />*/}
-		{/*<ApplyGem />*/}
+class App extends Component {
+	constructor(props) {
+		super();
+	}
 
-		<Card nature="text">
-			<h4>Smaller Projects</h4>
-			<ul>
-				<li>Niles Yoga</li>
-				<li>Songwriter's Studio</li>
-				<li>Conservation Centers for Species Survival</li>
-				<li></li>
-			</ul>
-		</Card>
-	</div>
-);
+	render() {
+		return(
+			<div>
+				<Card nature="text">
+					<h1>Cameron Steele</h1>
+					<h2>developer</h2>
+					<h3>designer</h3>
+					<a className="email" href="mailto:cameronsteele@steelecameron.com">
+						cameronsteele@steelecameron.com
+					</a><br />
+					{/*<!-- <a className="phone" href="tel:+13479797266">347-979-7266</a> -->*/}
+					{/*<!-- <a href="https://github.com/cameronsteele"><i className="fab fa-github"></i></a> -->*/}
+				</Card>
+
+				<KINDSnacks />
+				<CloudCred />
+				<Telephenesis />
+				<FoundSoundNation />
+				<EchoSign />
+				<ColonieNYC />
+				{/*<BismuthBear />*/}
+				{/*<GoldenCeph />*/}
+				{/*<ApplyGem />*/}
+
+				<Card nature="text">
+					{/*<h4>Smaller Projects</h4>
+					<ul>
+						<li>Niles Yoga</li>
+						<li>Songwriter's Studio</li>
+						<li>Conservation Centers for Species Survival</li>
+					</ul>*/}
+				</Card>
+			</div>
+		);
+	}
+}
 
 export default App;
